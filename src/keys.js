b637efba369e5b96cb9a94f01ed66af4dfe10b66
ErikSosa1996 <202001709@udv.edu.gt>
2022-04-25 12:54:02 -0600

    Creacion de repositorio local

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'esosa',
+        password: 'sosa1234',
+        database: 'db_links'
+    }
+
+};
